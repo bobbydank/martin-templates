411d79c40f90599e921c208cbbae7546d7ed2f3d
Robert Danklefsen <robert.danklefsen@barn3s.com>
2022-04-18 19:40:31 -0500

    Removed api key

--- assets/js/mapbox.js
+++ assets/js/mapbox.js
@@ -5,16 +5,7 @@ var currentZoom = 1;
 var defaultZoom = 3;
 var flyActive = false;
 var groups = new Array();
-var geocodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAaKhiy9B-oKtOriGav7CH753YRX5l8qvM&address=';
-
-mapboxgl.accessToken = 'pk.eyJ1IjoianotYmFybjNzIiwiYSI6ImNsMjU0ZTYxajIwZ20zZG1pcWN0bTFscjMifQ.ZiCT27OtewmE_T4fRxvC0w';
-var map = new mapboxgl.Map({
-	container: 'mapbox',
-	style: 'mapbox://styles/jz-barn3s/cl254ifao002r16p1dbw7d4aa', 
-	zoom: currentZoom, // starting zoom 
-});
-
-//map.scrollZoom.disable();
+var map = null;
 
 //the magic
 window.onload = function () {
@@ -24,6 +15,16 @@ window.onload = function () {
 
     mapboxSingle.forEach((container) => {
         console.log('hi');
+
+        mapboxgl.accessToken = 'pk.eyJ1IjoianotYmFybjNzIiwiYSI6ImNsMjU0ZTYxajIwZ20zZG1pcWN0bTFscjMifQ.ZiCT27OtewmE_T4fRxvC0w';
+        map = new mapboxgl.Map({
+            container: 'mapbox',
+            style: 'mapbox://styles/jz-barn3s/cl254ifao002r16p1dbw7d4aa', 
+            zoom: currentZoom, // starting zoom 
+        });
+
+        //map.scrollZoom.disable();
+
         //var markers = container.getElementsByClassName('marker');
         //console.log(markers);
     });
