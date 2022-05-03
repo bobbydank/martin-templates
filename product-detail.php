740703f5e207764eef30f58551675a79da5ef2a5
Robert Danklefsen <robert.danklefsen@barn3s.com>
2022-05-03 14:03:40 -0500

    Added Product Detail page

---
+++ product-detail.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include '_head.php'; ?>
+
+<body>
+  <?php include '_header.php'; ?>
+
+  <!-- Page Hero -->
+  <div class="page-hero" style="background-image:url('/assets/imgs/heroes/power-transmission.jpg');">
+    <section class="py-12 md:py-20 clearfix max-w-screen-2xl mx-auto w-11/12 secondary-font">
+      <h1 class="text-white text-right md:text-5xl my-0">Spur Gears</h1>
+    </section>
+  </div>
+  <!-- End Page Hero -->
+
+  <!-- Main
+  This is will be the main content area. Header and Footer should be the same on all pages. -->
+  <main id="main" class="pt-8 pb-24">
+    <section class="clearfix max-w-screen-2xl mx-auto w-11/12 md:flex justify-end secondary-font mb-16">
+      <ul class="inline-list carrot-seperator uppercase">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/power-transmission.php">Power Transmission</a>
+        </li>
+        <li>
+          <a href="/power-transmission-gears.php">Gears</a>
+        </li>
+        <li>
+          <a href="#">Spur Gears</a>
+        </li>
+        <li>
+          <a href="#">Gear Spur 14 1/2 DEG Cast C10100</a>
+        </li>
+      </ul>
+    </section>
+
+    <section class="clearfix max-w-screen-2xl mx-auto w-11/12 md:w-9/12 secondary-font mb-20 pt-16" data-aos="fade-in">
+      <div class="md:mx-8 text-center">
+        <h2 class="text-primary font-bold">Product Detail</h2>
+        
+      </div>
+    </section>
+
+  </main>
+  <!-- End Main -->
+
+  <?php include '_footer.php'; ?>
+
+</body>
+</html>
