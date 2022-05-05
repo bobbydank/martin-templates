43fb3212b9c8607ebee5e814b7daa459c35919af
Robert Danklefsen <robert.danklefsen@barn3s.com>
2022-05-05 15:20:31 -0500

    Created locations page

---
+++ locations.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include '_head.php'; ?>
+
+<body>
+  <?php include '_header.php'; ?>
+
+  <!-- Page Hero -->
+  <div class="page-hero" style="background-image:url('/assets/imgs/heroes/company.jpg');">
+    <section class="py-12 md:py-20 clearfix max-w-screen-2xl mx-auto w-11/12 secondary-font">
+      <h1 class="text-white text-right md:text-5xl my-0">Company</h1>
+    </section>
+  </div>
+  <!-- End Page Hero -->
+
+  <!-- Main
+  This is will be the main content area. Header and Footer should be the same on all pages. -->
+  <main id="main" class="pt-8 pb-24">
+    <section class="clearfix max-w-screen-2xl mx-auto w-11/12 md:flex justify-end secondary-font mb-16">
+      <ul class="inline-list carrot-seperator uppercase">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/company.php">Company</a>
+        </li>
+        <li>
+          <a href="/locations.php">Locations</a>
+        </li>
+      </ul>
+    </section>
+
+    <section class="clearfix max-w-screen-lg mx-auto w-11/12 md:w-9/12 secondary-font mb-20" data-aos="fade-in">
+      <div class="md:mx-8">
+        <h2 class="text-primary text-center">History</h2>
+        <p class="text-2xl leading-9">Martin Sprocket & Gear was founded in 1951 by Joe Martin Sr. Over the decades Martin has expanded through machining, fabrication, forging, casting, powdered metal technology and machined & injection molded plastic of power transmission components, material handling products and industrial hand tools.</p>
+        <p>Martin has grown to include manufacturing and sales facilities strategically located worldwide. Martin's philosophy is simple. "Make a quality product, have plenty of it available and be easy to do business with!" Martin accomplishes this by combining the convenience of regional & branch manufacturing plants with the strength of our distributors.</p>
+        <p>In the power transmission, bulk material handling, industrial hand tools and conveyor pulley industries, there's one company that stands apart—it's the one with the reputation for exceptional customer service—the one with parts and materials in stock nationwide—the one that provides quick turnaround times on MTOs and alterations—the one that keeps operations up and running with minimum downtime.</p>
+        <p>Over the years, Martin has proven to be easy to do business with, and Martin's philosophy of providing quality products, exceptional inventory and service “second to none” has proved to be a successful and innovative combination. As a result of this philosophy, Martin has grown to be a world-class leader in the industrial marketplace.</p>
+        <p>Every Martin plant in North America has complete product line inventory of the entire product line. And, every plant has the ability to manufacture or custom alter parts with complete machining. Finished product can be staged locally for release per customer schedule.</p>
+      </div>
+    </section>
+
+    
+  </main>
+  <!-- End Main -->
+
+  <?php include '_footer.php'; ?>
+
+</body>
+</html>
