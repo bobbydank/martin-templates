06cf918c5a919b1c77650ab3258d7efa6451d753
Robert Danklefsen <robert.danklefsen@barn3s.com>
2022-05-25 15:32:42 -0500

    Started on Forgings page

---
+++ forgings.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include '_head.php'; ?>
+
+<body>
+  <?php include '_header.php'; ?>
+
+  <!-- Page Hero -->
+  <div class="page-hero" style="background-image:url('/assets/imgs/heroes/power-transmission.jpg');">
+    <section class="py-12 md:py-20 clearfix max-w-screen-2xl mx-auto w-11/12 secondary-font">
+      <h1 class="text-white text-right md:text-5xl my-0">Forgings</h1>
+    </section>
+  </div>
+  <!-- End Page Hero -->
+
+  <!-- Main
+  This is will be the main content area. Header and Footer should be the same on all pages. -->
+  <main id="main" class="pt-8 pb-24">
+    <section class="clearfix max-w-screen-2xl mx-auto w-11/12 md:flex justify-end secondary-font mb-16">
+      <ul class="inline-list carrot-seperator uppercase">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/company.php">Company</a>
+        </li>
+        <li>
+          <a href="/forgings.php">Forgings</a>
+        </li>
+      </ul>
+    </section>
+
+    <section class="clearfix max-w-screen-lg mx-auto w-11/12 md:w-9/12 secondary-font mb-20" data-aos="fade-in">
+      <div class="md:mx-8">
+        <h2 class="text-primary text-center">Forging Expertise Since 1917.</h2>
+        <p>Martin Tool and Forge, located in Fort Worth, Texas, has been a leading supplier of quality American forged products since 1917. Martin forgings are well regarded for Quality, Innovation, Reliability & Cost Savings.</p>
+        <p>Know how garnered over many decades provides unsurpassed benefit for the industrial user of custom forgings. This includes all phases in the forging process - die design and engineering, forging, coining, heat treating, and polishing. Hammers ranging in size from 1,000 to 5,000 pounds produce finished parts from a few ounces to 50 pounds. Various alloys, considerable capacity, and secondary machining capability enable Martin to deliver near 100% density requirements in a cost effective manner.</p>
+      </div>
+    </section>
+
+    <section class="clearfix max-w-screen-lg mx-auto w-11/12 md:w-9/12 mb-32">
+      <iframe src="https://player.vimeo.com/video/191553905?h=87a8a54dc0&title=0&byline=0&portrait=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+    </section>
+
+    
+  </main>
+  <!-- End Main -->
+
+  <?php include '_footer.php'; ?>
+
+</body>
+</html>
