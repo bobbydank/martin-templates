58cb7c244a56931b386033db0664435c09e42520
Robert Danklefsen <robert.danklefsen@barn3s.com>
2022-05-26 13:54:26 -0500

    Finished Forging page

--- forgings.php
+++ forgings.php
@@ -39,13 +39,97 @@
       </div>
     </section>
 
-    <section class="clearfix max-w-screen-lg mx-auto w-11/12 md:w-9/12 mb-32 b3-dark-shadow">
+    <section class="clearfix max-w-screen-lg mx-auto w-11/12 md:w-9/12 mb-32 b3-dark-shadow" data-aos="fade-in">
       <div class="video-container">
         <iframe src="https://player.vimeo.com/video/191553905?h=87a8a54dc0&title=0&byline=0&portrait=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
       </div>
     </section>
 
+    <div class="darkgrey-bg py-32 mb-16">
+      <section class="max-w-2xl mx-auto w-11/12 text-center mb-20" data-aos="fade-in">
+        <h2 class="text-primary">Manufacturing Capabilities</h2>
+        <p class="text-black">Martin utilizes closed impression die forging where two dies containing the impression of the shape are brought together deforming the metal. Martin provides two types of closed impression die forgings: Hammer Forgings and Press Forgings.</p>
+      </section>
+      <section class="md:flex items-start gap-20 max-w-screen-lg mx-auto w-11/12 md:w-9/12 mb-12">
+        <div class="w-full md:w-1/3 text-center md:text-left" data-aos="fade-right">
+          <h4 class="text-primary mb-8">Hammer Forging</h4>
+          <p class="mb-12">Martin utilizes closed impression die forging where two dies containing the impression of the shape are brought together deforming the metal. Martin provides two types of closed impression die forgings: Hammer Forgings and Press Forgings.</p>
+        </div>
+        <div class="w-full md:w-2/3" data-aos="fade-left">
+          <img src="/assets/imgs/forging-one.png" alt="Martin Learning" class="w-full" />
+        </div>
+      </section>
+      <section class="md:flex items-start gap-20 max-w-screen-lg mx-auto w-11/12 md:w-9/12">
+        <div class="w-full md:w-1/3 text-center md:text-left" data-aos="fade-right">
+          <h4 class="text-primary mb-8">Press Forging</h4>
+          <p class="mb-12">The stock is usually hit only once in each die impression. Increased deformation and control achieved through press forging will give the material better consistency of properties.</p>
+        </div>
+        <div class="w-full md:w-2/3" data-aos="fade-left">
+          <img src="/assets/imgs/forging-two.png" alt="Martin Learning" class="w-full" />
+        </div>
+      </section>
+    </div>
     
+    <div class="darkgrey-bg pt-32 pb-24 mb-16">
+      <h2 class="text-primary text-center" data-aos="fade-in">Custom Capabilities</h2>
+      <section class="lg:flex items-start gap-20 max-w-screen-2xl mx-auto w-11/12 md:w-9/12 mb-16 secondary-font">
+        <div class="lg:w-1/3 mb-12" data-aos="fade-right">
+          <h4 class="text-primary">Benefits</h4>
+          <ul>
+            <li>High impact strength and structural integrity</li>
+            <li>Extremely high consistency of material and dimensions of parts</li>
+            <li>Higher strength to weight ratios reduce both weight and size when close fit or weight issues are a factor</li>
+            <li>Forging can produce complex shapes that otherwise may require multiple manufacturing processes</li>
+            <li>Forged parts are compatible with most secondary operations such as heat treating, machining and fabrication</li>
+            <li>In many cases one forged part can be created where multiple parts were originally used, reducing labor cost</li>
+            <li>Martin's dedication to quality and service, is second to none</li>
+          </ul>
+        </div>
+        <div class="lg:w-1/3 mb-12" data-aos="fade-up">
+          <h4 class="text-primary">Secondary Operations</h4>
+          <p>Other metalworking and inspection processes complement the forging of carbon, alloy and stainless steel components and parts.</p>
+          <ul>
+            <li>Heat Treatment</li>
+            <li>CNC Machining, Broaching</li>
+            <li>Magnetic Particle Inspection</li>
+            <li>Liquid Penetrant Inspection</li>
+            <li>Grinding, Polishing</li>
+            <li>Cleaning</li>
+            <li>Plating or Coating</li>
+            <li>Coining</li>
+          </ul>
+        </div>
+        <div class="mb-12 lg:w-1/3" data-aos="fade-left">
+          <h4 class="text-primary">Material Capabilities</h4>
+          <p><strong>Hammers<br />Up to 20" length</strong></p>
+          <div class="flex gap-1">
+            <p class="w-1/2 mb-0">Carbon Steel</p>
+            <p class="w-1/2 mb-0 text-right">.25 to 45 lb</p>
+          </div>
+          <div class="flex gap-1">
+            <p class="w-1/2 mb-0">Alloy Steel</p>
+            <p class="w-1/2 mb-0 text-right">.25 to 45 lb</p>
+          </div>
+          <div class="flex gap-1 mb-10">
+            <p class="w-1/2 mb-0">Stainless Steel </p>
+            <p class="w-1/2 mb-0 text-right">.15 to 25 lb</p>
+          </div>
+          <p><strong>Presses<br />Up to 12" diameter or 14" length</strong></p>
+          <div class="flex gap-1">
+            <p class="w-1/2 mb-0">Carbon Steel</p>
+            <p class="w-1/2 mb-0 text-right">1.5 to 40 lb</p>
+          </div>		
+          <div class="flex gap-1">
+            <p class="w-1/2 mb-0">Alloy Steel</p>
+            <p class="w-1/2 mb-0 text-right">1.5 to 40 lb</p>
+          </div>
+          <div class="flex gap-1">
+            <p class="w-1/2 mb-0">Stainless Steel</p>
+            <p class="w-1/2 mb-0 text-right">1.5 to 20 lb</p>
+          </div>
+        </div>
+      </section>
+    </div>
   </main>
   <!-- End Main -->
 
