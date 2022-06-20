74431a050e4381295a9e8c3f2321d8699a8f156e
Robert Danklefsen <robert.danklefsen@barn3s.com>
2022-06-20 14:20:29 -0500

    Finished page

---
+++ catalogs/power-transmission.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include '../_head.php'; ?>
+
+<body>
+  <?php include '../_header.php'; ?>
+
+  <!-- Page Hero -->
+  <div class="page-hero" style="background-image:url('/assets/imgs/heroes/resources.jpg');">
+    <section class="py-12 md:py-20 clearfix max-w-screen-2xl mx-auto w-11/12 secondary-font">
+      <h1 class="text-white text-right md:text-5xl my-0">Resources</h1>
+    </section>
+  </div>
+  <!-- End Page Hero -->
+
+  <!-- Main
+  This is will be the main content area. Header and Footer should be the same on all pages. -->
+  <main id="main" class="pt-8 pb-24">
+    <section class="clearfix max-w-screen-2xl mx-auto w-11/12 md:flex justify-end secondary-font mb-16">
+      <ul class="inline-list carrot-seperator uppercase">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/resources.php">Resources</a>
+        </li>
+        <li>
+          <a href="#">Literature and Media</a>
+        </li>
+        <li>
+          <a href="/catalogs.php">Catalogs</a>
+        </li>
+        <li>
+          <a href="/catalogs.php">Power Transmission</a>
+        </li>
+      </ul>
+    </section>
+
+    <section class="clearfix max-w-screen-md mx-auto w-11/12 md:w-9/12 secondary-font mb-20" data-aos="fade-in">
+      <h2 class="text-primary text-center mb-16">Catalogs</h2>
+      <ul class="inline-list text-center no-pad-a line-seperator pad-more no-leading monitor-last-item">
+        <li><a href="/catalogs/power-transmission.php" class="active">Power Transmission</a></li>
+        <li><a href="/catalogs/material-handling.php">Material Handling</a></li>
+        <li><a href="/catalogs/idlers.php">Idlers</a></li>
+        <li><a href="/catalogs/heavy-duty.php">Heavy-Duty Conveyor Pulleys</a></li>
+        <li><a href="/catalogs/custom-products.php">Custom Products</a></li>
+        <li><a href="/catalogs/engineering.php">Engineering</a></li>
+        <li><a href="/catalogs/industrial.php">Industrial Hand Tools</a></li>
+        <li><a href="/catalogs/plastics.php">Plastics</a></li>
+      </ul>
+    </section>
+
+    <section class="clearfix max-w-screen-2xl mx-auto" data-aos="fade-in">
+      <div class="mb-5">
+        <ul class="downloads-list">
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/_power%20transmission%20catalog/the-big-catalog-(full).pdf">
+              <p>The Big Catalog (Full)</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+        </ul> 
+      </div>
+      <div class="list-dropper mb-5">
+        <span>Sprocket Catalog</span>
+        <ul class="downloads-list">
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/2_sprocket%20catalog/800-series-conveyor-sprockets.pdf">
+              <p>800 Series Conveyor Sprockets</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/2_sprocket%20catalog/engineering-class-sprockets.pdf">
+              <p>Engineering Class Sprockets</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/2_sprocket%20catalog/idler-sprockets.pdf">
+              <p>Idler Sprockets</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/2_sprocket%20catalog/metric-roller-chain-sprockets.pdf">
+              <p>Metric Roller Chain Sprockets</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/2_sprocket%20catalog/roller-chain-sprockets.pdf">
+              <p>Roller Chain Sprockets</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="list-dropper mb-5">
+        <span>Sprocket Catalog</span>
+        <ul class="downloads-list">
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/3_bushing%20catalog/bushing-catalog.pdf">
+              <p>Martin's Bushing Catalog</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/3_bushing%20catalog/mst-bushings-catalog.pdf">
+              <p>MST® Bushings Catalog</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/3_bushing%20catalog/qd-bushings-catalog.pdf">
+              <p>QD Bushings Catalog</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/3_bushing%20catalog/qd-mst-idler-bushings.pdf">
+              <p>QD &MST® Idler Bushings</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/3_bushing%20catalog/taper-bushings.pdf">
+              <p>Taper Bushings</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/3_bushing%20catalog/weld-on-hubs.pdf">
+              <p>Weld-On Hubs</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="list-dropper mb-5">
+        <span>Bushing Catalog</span>
+        <ul class="downloads-list">
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/3_bushing%20catalog/bushing-catalog.pdf">
+              <p>Martin's Bushing Catalog</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/3_bushing%20catalog/mst-bushings-catalog.pdf">
+              <p>MST® Bushings Catalog</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/3_bushing%20catalog/qd-bushings-catalog.pdf">
+              <p>QD Bushings Catalog</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/3_bushing%20catalog/qd-mst-idler-bushings.pdf">
+              <p>QD &MST® Idler Bushings</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/3_bushing%20catalog/taper-bushings.pdf">
+              <p>Taper Bushings</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/3_bushing%20catalog/weld-on-hubs.pdf">
+              <p>Weld-On Hubs</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="list-dropper mb-5">
+        <span>Gear Catalog</span>
+        <ul class="downloads-list">
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/4_gear%20catalog/bevel-gears.pdf">
+              <p>Bevel Gears</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/4_gear%20catalog/gear-rack.pdf">
+              <p>Gear Rack</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/4_gear%20catalog/miter-gears.pdf">
+              <p>Miter Gears</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/4_gear%20catalog/spur-gears.pdf">
+              <p>Spur Gears</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/4_gear%20catalog/worm-worm-gears.pdf">
+              <p>Worm &amp; Worm Gears</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="list-dropper mb-5">
+        <span>Synchronous Sprockets Catalog</span>
+        <ul class="downloads-list">
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/5_synchronous%20sprockets%20catalog/high-hp-hts-sprockets.pdf">
+              <p>High HP HTS® Sprockets</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/5_synchronous%20sprockets%20catalog/hts-sprockets.pdf">
+              <p>HTS® Sprockets</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/5_synchronous%20sprockets%20catalog/mpc-sprockets.pdf">
+              <p>MPC® Sprockets</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/5_synchronous%20sprockets%20catalog/timing-pulleys.pdf">
+              <p>Timing Pulleys</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="list-dropper mb-5">
+        <span>Sheaves Catalog</span>
+        <ul class="downloads-list">
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/6_sheaves%20catalog/fhp-sheaves.pdf">
+              <p>FHP Sheaves</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/6_sheaves%20catalog/mst-sheaves.pdf">
+              <p>MST® Sheaves</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/6_sheaves%20catalog/qd-sheaves.pdf">
+              <p>QD Sheaves</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/6_sheaves%20catalog/sheave-catalog.pdf">
+              <p>Martin's Sheave Catalog</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/6_sheaves%20catalog/taper-bushed-sheaves.pdf">
+              <p>Taper Bushed Sheaves</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/6_sheaves%20catalog/variable-pitch-sheaves.pdf">
+              <p>Variable Pitch Sheaves</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="list-dropper mb-5">
+        <span>Coupling Catalog</span>
+        <ul class="downloads-list">
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/7_coupling%20catalog/blue-flex-couplings.pdf">
+              <p>Blue-Flex® Couplings</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/7_coupling%20catalog/go-flex.pdf">
+              <p>Go-Flex® Couplings</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/7_coupling%20catalog/jaw-couplings.pdf">
+              <p>Jaw Couplings</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/7_coupling%20catalog/martin-flex-couplings.pdf">
+              <p>Martin-Flex® Couplings</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/7_coupling%20catalog/quadra-flex-couplings.pdf">
+              <p>Quadra-Flex® Couplings</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.martinsprocket.com/docs//catalogs/power%20transmission/7_coupling%20catalog/roller-chain-couplings.pdf">
+              <p>Roller Chain Couplings</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </main>
+  <!-- End Main -->
+
+  <?php include '../_footer.php'; ?>
+</body>
+</html>
