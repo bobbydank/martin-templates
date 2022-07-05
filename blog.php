a2b9a08fdb48509735e0d3ce82aab8b2950ae740
Robert Danklefsen <robert.danklefsen@barn3s.com>
2022-07-05 13:22:11 -0500

    Finished blog page

---
+++ blog.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include '_head.php'; ?>
+
+<body>
+  <?php include '_header.php'; ?>
+
+  <!-- Page Hero -->
+  <div class="page-hero" style="background-image:url('/assets/imgs/heroes/blog.jpg');">
+    <section class="py-12 md:py-20 clearfix max-w-screen-2xl mx-auto w-11/12 secondary-font">
+      <h1 class="text-white text-right md:text-5xl my-0">Blog</h1>
+    </section>
+  </div>
+  <!-- End Page Hero -->
+
+  <!-- Main
+  This is will be the main content area. Header and Footer should be the same on all pages. -->
+  <main id="main" class="pt-8 pb-24">
+    <section class="clearfix max-w-screen-2xl mx-auto w-11/12 md:flex justify-end secondary-font mb-16">
+      <ul class="inline-list carrot-seperator uppercase">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/blog.php">Blog</a>
+        </li>
+      </ul>
+    </section>
+
+    <section class="clearfix max-w-screen-lg mx-auto w-11/12 md:w-9/12 secondary-font mb-20" data-aos="fade-in">
+      <div class="md:mx-8 text-center">
+        <h2 class="text-primary">Helpful Info and Practical Tips for Industrial Applications</h2>
+        <ul class="inline-list text-center no-pad-a line-seperator pad-more no-leading monitor-last-item">
+          <li><a href="#">All Blogs</a></li>
+          <li><a href="#">Power Transmission</a></li>
+          <li><a href="#" class="active">Material Handling</a></li>
+          <li><a href="#">Hand Tools</a></li>
+          <li><a href="#">Conveyor Pulleys</a></li>
+          <li><a href="#">Custom Products</a></li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="max-w-screen-2xl pb-40 mx-auto w-11/12">
+      <div class="flex items-center flex-wrap items-stretch grid-borders">
+        <div class="flex flex-col md:p-16 p-10 no-td w-full md:w-1/2 lg:w-1/3 text-center text-xl relative" data-aos="fade-up">
+          <img src="/assets/imgs/blog/bucket-components.png" class="w-11/12 mx-auto mb-14" alt="Bucket Components">
+          <h4 class="text-primary text-center">
+            Bucket Components
+          </h4>
+          <p class="text-center mb-10 secondary-font">
+            After you have gone through the exercise of selecting and quoting the bucket for your system, what other items [...]
+          </p>
+          <div class="block mt-auto mb-5">
+            <a href="#" class="b3-button">Read</a>
+          </div>
+        </div>
+        <div class="flex flex-col md:p-16 p-10 no-td w-full md:w-1/2 lg:w-1/3 text-center text-xl relative" data-aos="fade-up">
+          <img src="/assets/imgs/blog/non-metallic-buckets.png" class="w-11/12 mx-auto mb-14" alt="Non-Metallic Buckets">
+          <h4 class="text-primary text-center">
+            Non-Metallic Buckets
+          </h4>
+          <p class="text-center mb-10 secondary-font">
+            You may have wondered: why use a molded non-metallic bucket? Light weight, Reduction in energy [...]
+          </p>
+          <div class="block mt-auto mb-5">
+            <a href="#" class="b3-button">Read</a>
+          </div>
+        </div>
+        <div class="flex flex-col md:p-16 p-10 no-td w-full md:w-1/2 lg:w-1/3 text-center text-xl relative" data-aos="fade-up">
+          <img src="/assets/imgs/blog/cast-iron-elevator-buckets.png" class="w-11/12 mx-auto mb-14" alt="Cast Iron Elevator Buckets">
+          <h4 class="text-primary text-center">
+            Cast Iron Elevator Buckets
+          </h4>
+          <p class="text-center mb-10 secondary-font">
+            We recently got asked: why use a cast iron elevator bucket? Good abrasion and impact resistance [...]
+          </p>
+          <div class="block mt-auto mb-5">
+            <a href="#" class="b3-button">Read</a>
+          </div>
+        </div>
+        <div class="flex flex-col md:p-16 p-10 no-td w-full md:w-1/2 lg:w-1/3 text-center text-xl relative" data-aos="fade-up">
+          <img src="/assets/imgs/blog/fabricated-metallic-buckets.png" class="w-11/12 mx-auto mb-14" alt="Fabricated Metallic Buckets">
+          <h4 class="text-primary text-center">
+            Fabricated Metallic Buckets
+          </h4>
+          <p class="text-center mb-10 secondary-font">
+            For more than 50 years, Martin has specialized in Bucket Elevators and other material handling solutions [...] 
+          </p>
+          <div class="block mt-auto mb-5">
+            <a href="#" class="b3-button">Read</a>
+          </div>
+        </div>
+        <div class="flex flex-col md:p-16 p-10 no-td w-full md:w-1/2 lg:w-1/3 text-center text-xl relative" data-aos="fade-up">
+          <img src="/assets/imgs/blog/you-need-buckets.png" class="w-11/12 mx-auto mb-14" alt="You Need Buckets">
+          <h4 class="text-primary text-center">
+            You Need Buckets
+          </h4>
+          <p class="text-center mb-10 secondary-font">
+            Although a Bucket Elevator can use a Belt or a Chain, it will always use a Bucket. There is a bucket for every [...]  
+          </p>
+          <div class="block mt-auto mb-5">
+            <a href="#" class="b3-button">Read</a>
+          </div>
+        </div>
+        <div class="flex flex-col md:p-16 p-10 no-td w-full md:w-1/2 lg:w-1/3 text-center text-xl relative" data-aos="fade-up">
+          <img src="/assets/imgs/blog/bucket-elevator-series-vi.png" class="w-11/12 mx-auto mb-14" alt="Bucket Elevator Series Part IV">
+          <h4 class="text-primary text-center">
+            Bucket Elevator Series Part IV
+          </h4>
+          <p class="text-center mb-10 secondary-font">
+            The Martin bucket list is something to live for! Read below to see why all other buckets pale in comparison [...]  
+          </p>
+          <div class="block mt-auto mb-5">
+            <a href="#" class="b3-button">Read</a>
+          </div>
+        </div>
+        <div class="flex flex-col md:p-16 p-10 no-td w-full md:w-1/2 lg:w-1/3 text-center text-xl relative" data-aos="fade-up">
+          <img src="/assets/imgs/blog/bucket-elevator-series-iii.png" class="w-11/12 mx-auto mb-14" alt="Bucket Elevator Series Part III">
+          <h4 class="text-primary text-center">
+            Bucket Elevator Series Part III
+          </h4>
+          <p class="text-center mb-10 secondary-font">
+            Is your Bucket Elevator not up to snuff? Martin has just the right stuff. Our Super Capacity bucket elevators [...] 
+          </p>
+          <div class="block mt-auto mb-5">
+            <a href="#" class="b3-button">Read</a>
+          </div>
+        </div>
+        <div class="flex flex-col md:p-16 p-10 no-td w-full md:w-1/2 lg:w-1/3 text-center text-xl relative" data-aos="fade-up">
+          <img src="/assets/imgs/blog/bucket-elevator-series-ii.png" class="w-11/12 mx-auto mb-14" alt="Bucket Elevator Series Part II">
+          <h4 class="text-primary text-center">
+            Bucket Elevator Series Part II
+          </h4>
+          <p class="text-center mb-10 secondary-font">
+            Sugar, spice, and everything rice, Martin has the Bucket Elevator you have been looking for. We provide a [...] 
+          </p>
+          <div class="block mt-auto mb-5">
+            <a href="#" class="b3-button">Read</a>
+          </div>
+        </div>
+        <div class="flex flex-col md:p-16 p-10 no-td w-full md:w-1/2 lg:w-1/3 text-center text-xl relative" data-aos="fade-up">
+          <img src="/assets/imgs/blog/bucket-elevator-series-i.png" class="w-11/12 mx-auto mb-14" alt="Bucket Elevator Series Part I">
+          <h4 class="text-primary text-center">
+            Bucket Elevator Series Part I
+          </h4>
+          <p class="text-center mb-10 secondary-font">
+            If your Bucket Elevator sticks or splits, call the ones with over 8 decades of satisfied customers. Martin will [...]  
+          </p>
+          <div class="block mt-auto mb-5">
+            <a href="#" class="b3-button">Read</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <!-- End Main -->
+
+  <?php include '_footer.php'; ?>
+
+</body>
+</html>
