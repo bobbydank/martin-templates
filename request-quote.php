642e4e422f7bbdfccf05ec74ed2e7ab32a159732
Robert Danklefsen <robert.danklefsen@barn3s.com>
2022-08-16 09:56:45 -0500

    80% done

---
+++ request-quote.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include '_head.php'; ?>
+
+<body>
+  <?php include '_header.php'; ?>
+
+  <!-- Page Hero -->
+  <div class="page-hero" style="background-image:url('/assets/imgs/heroes/training.jpg');">
+    <section class="py-12 md:py-20 clearfix max-w-screen-2xl mx-auto w-11/12 secondary-font">
+      <h1 class="text-white text-right md:text-5xl my-0">Request A Quote</h1>
+    </section>
+  </div>
+  <!-- End Page Hero -->
+
+  <!-- Main
+  This is will be the main content area. Header and Footer should be the same on all pages. -->
+  <main id="main" class="pt-8 pb-24">
+    <section class="clearfix max-w-screen-2xl mx-auto w-11/12 md:flex justify-end secondary-font mb-16">
+      <ul class="inline-list carrot-seperator uppercase">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/request-quote.php">Request A Quote</a>
+        </li>
+      </ul>
+    </section>
+
+    <section class="clearfix max-w-screen-lg mx-auto w-11/12 md:w-9/12 secondary-font mb-20" data-aos="fade-in">
+      <div class="md:mx-8 text-center">
+        <h2 class="text-primary">Online Quote Request Tool</h2>
+        <p>We look forward to quoting your power transmission parts, material handling projects or your hand tools needs. To use our online quote request tool, simply select your product below, enter the details of your project, and then a friendly Martin representative will be in touch right away with pricing and delivery time-frames. </p>
+        <div class="max-w-screen-md mx-auto mt-16">
+          <h4 class="text-primary">Have a question about the specifications of your project? </h4>
+          <p>
+            Let one of our knowledgeable engineering experts assist you with the details. <br />
+            <a class="font-bold text-primary underline" href="#">Click here</a> to request more information or call us at <strong>817-258-3000</strong>.
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section class="clearfix max-w-screen-2xl- mx-auto w-11/12 md:w-10/12 secondary-font mt-36 mb-20" data-aos="fade-in">
+      <div class="md:flex justify-items-stretch">
+        <div class="md:w-1/2 lg:w-1/4 p-12">
+          <div class="rounded-xl primary-bg py-8 text-center w-5/6 mx-auto mb-8 mh-210 flex justify-center items-center">
+            <img src="/assets/imgs/part-number.svg" class="w-1/2 inline-block" alt="Part Number Interchange">
+          </div>
+          <h5 class="text-primary text-center mb-8">Part Number Interchange</h5>
+          <p class="leading-loose text-center">
+            
+          </p>
+        </div>
+        <div class="md:w-1/2 lg:w-1/4 p-12">
+          <div class="rounded-xl primary-bg py-8 text-center w-5/6 mx-auto mb-8 mh-210 flex justify-center items-center">
+            <img src="/assets/imgs/literature.svg" class="w-1/2 inline-block" alt="Literature and Media">
+          </div>  
+          <h5 class="text-primary text-center mb-8">Literature and Media</h5>
+          <p class="leading-loose text-center">
+            
+          </p>
+        </div>
+        <div class="md:w-1/2 lg:w-1/4 p-12">
+          <div class="rounded-xl primary-bg py-8 text-center w-5/6 mx-auto mb-8 mh-210 flex justify-center items-center">
+            <img src="/assets/imgs/hard-hat.svg" class="w-1/2 inline-block" alt="Safety and Installation">
+          </div>
+          <h5 class="text-primary text-center mb-8">Part Number Interchange</h5>
+          <p class="leading-loose text-center">
+            
+          </p>
+        </div>
+        <div class="md:w-1/2 lg:w-1/4 p-12">
+          <div class="rounded-xl primary-bg py-8 text-center w-5/6 mx-auto mb-8 mh-210 flex justify-center items-center">
+            <img src="/assets/imgs/literature.svg" class="w-1/2 inline-block" alt="Literature and Media">
+          </div>  
+          <h5 class="text-primary text-center mb-8">Literature and Media</h5>
+          <p class="leading-loose text-center">
+            
+          </p>
+        </div>
+      </div>
+    </section>
+
+  </main>
+  <!-- End Main -->
+
+  <?php include '_footer.php'; ?>
+
+</body>
+</html>
