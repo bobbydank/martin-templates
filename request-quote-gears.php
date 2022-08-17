0fb2a26b428168f2f9c7b040eb7c55876aeaf36b
Robert Danklefsen <robert.danklefsen@barn3s.com>
2022-08-17 11:24:27 -0500

    Started

---
+++ request-quote-gears.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include '_head.php'; ?>
+
+<body>
+  <?php include '_header.php'; ?>
+
+  <!-- Page Hero -->
+  <div class="page-hero" style="background-image:url('/assets/imgs/heroes/training.jpg');">
+    <section class="py-12 md:py-20 clearfix max-w-screen-2xl mx-auto w-11/12 secondary-font">
+      <h1 class="text-white text-right md:text-5xl my-0">Request A Quote</h1>
+    </section>
+  </div>
+  <!-- End Page Hero -->
+
+  <!-- Main
+  This is will be the main content area. Header and Footer should be the same on all pages. -->
+  <main id="main" class="pt-8 pb-24">
+    <section class="clearfix max-w-screen-2xl mx-auto w-11/12 md:flex justify-end secondary-font mb-16">
+      <ul class="inline-list carrot-seperator uppercase">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/request-quote.php">Request A Quote</a>
+        </li>
+        <li>
+          <a href="/request-quote-gears.php">Gears</a>
+        </li>
+      </ul>
+    </section>
+
+    <section class="clearfix max-w-screen-xl mx-auto w-11/12 md:w-9/12 secondary-font mb-20" data-aos="fade-in">
+      <div class="md:mx-8 text-center">
+        <h2 class="text-primary">Gears</h2>
+        <div class="md:flex justify-start">
+          <div class="md:w-2/3">
+            <img src="" class="w-full block" />
+          </div>
+          <div class="md:w-1/3 text-right">
+            <p class="font-bold">Dimensions</p>
+            <ul>
+              <li><span class="font-bold">1</span> - Bore Size</li>
+              <li><span class="font-bold">2</span> - Pitch Diameter (PD)</li>
+              <li><span class="font-bold">3</span> - Outside Diameter (OD)</li>
+              <li><span class="font-bold">4</span> - Face Width</li>
+              <li><span class="font-bold">5</span> - Hub Projection</li>
+              <li><span class="font-bold">6</span> - Hub Diameter (HOD)</li>
+              <li><span class="font-bold">7/8</span> - Shaft Centers / Mounting Distance</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+
+  </main>
+  <!-- End Main -->
+
+  <?php include '_footer.php'; ?>
+
+</body>
+</html>
