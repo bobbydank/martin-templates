2c5ff8a2aa5b6d9fe528bdcb0726825df4e5b45a
Robert Danklefsen <robert.danklefsen@barn3s.com>
2022-09-21 10:43:43 -0500

    finished

---
+++ literature-media.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include '_head.php'; ?>
+
+<body>
+  <?php include '_header.php'; ?>
+
+  <!-- Page Hero -->
+  <div class="page-hero" style="background-image:url('/assets/imgs/heroes/resources.jpg');">
+    <section class="py-12 md:py-20 clearfix max-w-screen-2xl mx-auto w-11/12 secondary-font">
+      <h1 class="text-white text-right md:text-5xl my-0">Resources</h1>
+    </section>
+  </div>
+  <!-- End Page Hero -->
+
+  <!-- Main
+  This is will be the main content area. Header and Footer should be the same on all pages. -->
+  <main id="main" class="pt-8 pb-24">
+    <section class="clearfix max-w-screen-2xl mx-auto w-11/12 md:flex justify-end secondary-font mb-16">
+      <ul class="inline-list carrot-seperator uppercase">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/resources.php">Resources</a>
+        </li>
+        <li>
+          <a href="#">Literature and Media</a>
+        </li>
+      </ul>
+    </section>
+
+    <section class="clearfix max-w-screen-md mx-auto w-11/12 md:w-9/12 secondary-font mb-10" data-aos="fade-in">
+      <h2 class="text-primary text-center">Literature and Media</h2>
+    </section>
+
+    <section class="clearfix max-w-screen-2xl mx-auto items-center w-11/12">
+      <div class="list-dropper up">
+        <span>Catalogs</span>
+        <ul class="downloads-list" style="display:block;">
+          <li>
+            <a href="#">
+              <p>Power Transmission</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Material Handling</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Idlers</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Heavy-Duty Conveyor Pulleys</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Custom Product</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Engineering</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Industrial Hand Tools</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Plastics</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="list-dropper up">
+        <span>Nomenclature</span>
+        <ul class="downloads-list">
+          <li>
+            <a href="#">
+              <p>Document 1</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Document 2</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="list-dropper up">
+        <span>Document Cost Savings</span>
+        <ul class="downloads-list">
+          <li>
+            <a href="#">
+              <p>Agriculture</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Automotive</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Building Materials</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Cat Litter</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Cement & Aggreagate</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Chemical</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Ethanol</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Fertilizer</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Food & Beverage</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>General Manufacturing</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Lumber & Wood</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Meat Products</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Mining</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Oil & Gas</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Oilseed</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Pet Products</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Power Generation</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Pulp & Paper</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Salt</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Recycling</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Rendering</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Waste Water</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="list-dropper up">
+        <span>Corporate Overview</span>
+        <ul class="downloads-list">
+          <li>
+            <a href="#">
+              <p>Martin's The ONE You Rely On Brochure</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="list-dropper up">
+        <span>Brochures</span>
+        <ul class="downloads-list">
+          <li>
+            <a href="#">
+              <p>Power Transmission</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Material Handling</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Idlers</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Heavy-Duty Conveyor Pulleys</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>General Brochures</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Industry Specific</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Custom Products</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Industrial Hand Tools</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Plastics</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="list-dropper up">
+        <span>Press Releases</span>
+        <ul class="downloads-list">
+          <li>
+            <a href="#">
+              <p>Press Release 1</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>Press Release 2</p>
+              <p>Download PDF</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </main>
+  <!-- End Main -->
+
+  <?php include '_footer.php'; ?>
+</body>
+</html>
