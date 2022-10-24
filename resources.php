60492aa05043fecacfe411c9d77fecee76978155
Robert Danklefsen <robert.danklefsen@barn3s.com>
2022-10-24 10:58:18 -0500

    Edits to Resources Page

--- resources.php
+++ resources.php
@@ -30,10 +30,10 @@
 
     <section class="clearfix max-w-screen-2xl mx-auto w-11/12 md:w-9/12 secondary-font mb-20" data-aos="fade-in">
       <div class="md:mx-8">
-        <h2 class="text-primary text-center mb-16">Customer Login</h2>
+        <h2 class="text-primary text-center mb-16">Resources</h2>
         <div class="md:flex justify-items-stretch gap-12">
           <div class="b3-shadow w-full md:w-1/2 px-12 pt-8 pb-12 mb-12 md:mb-0">
-            <h5 class="text-primary text-center">Order Status</h5>
+            <h5 class="text-primary text-center">Customer Login</h5>
             <form action="" method="" enctype="multipart/form-data">
               <input type="text" placeholder="User Name" name="" required>
               <input type="password" placeholder="Password" name="" required>
@@ -48,7 +48,7 @@
             </form>
           </div>
           <div class="b3-shadow w-full md:w-1/2 px-12 pt-8 pb-12">
-            <h5 class="text-primary text-center">Check Stock</h5>
+            <h5 class="text-primary text-center">Account Maintenance</h5>
             <form action="" method="" enctype="multipart/form-data">
               <input type="text" placeholder="User Name" name="" required>
               <input type="password" placeholder="Password" name="" required>
@@ -60,51 +60,18 @@
                   <img src="/assets/imgs/captcha.jpg" class="block w-full" alt="">
                 </div>
               </div>
+              <p class="text-center py-5">
+                <a href="#">Francais</a> | <a href="#">Espanol</a> | <a href="#">English</a>
+              </p>
             </form>
           </div>
         </div>
       </div>
     </section>
 
-    <section class="clearfix max-w-screen-2xl mx-auto w-11/12 md:w-9/12 secondary-font mb-20" data-aos="fade-in">
-      <div class="md:mx-8">
-        <div class="md:flex justify-items-stretch gap-12">
-          <div class="b3-shadow w-full md:w-1/2 px-12 pt-8 pb-12">
-            <h5 class="text-primary text-center">Account Maintenance</h5>
-            <form action="" method="" enctype="multipart/form-data" class="mb-8">
-              <input type="text" placeholder="User Name" name="" required>
-              <input type="password" placeholder="Password" name="" required>
-              <div class="flex mt-4">
-                <div class="w-1/2">
-                  <input type="submit" value="Submit" class="submit-button smaller">
-                </div>
-                <div class="w-1/2 justify-end flex">
-                  <img src="/assets/imgs/captcha.jpg" class="block w-full" alt="">
-                </div>
-              </div>
-            </form>
-            <ul class="inline-list text-center line-seperator pad-more no-leading">
-              <li>
-                <a href="#">Francais</a>
-              </li>
-              <li>
-                <a href="#">Espanol</a>
-              </li>
-              <li>
-                <a href="#">English</a>
-              </li>
-            </ul>
-          </div>
-          <div class="w-full md:w-1/2 flex">
-            <div class="flex justify-center w-full items-center">
-              <p class="text-center py-12">
-                For login asstistance, please <br />contact the marketing <br />department at <a href="tel:+18172583000" class="font-bold">817-258-3000</a>.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <p class="text-center pt-12 text-2xl font-fira">
+      For login asstistance, please contact the marketing department at <a href="tel:+18172583000" class="font-semibold">817-258-3000</a>.
+    </p>
 
     <section class="clearfix max-w-screen-2xl- mx-auto w-11/12 md:w-10/12 secondary-font mt-36 mb-20" data-aos="fade-in">
       <div class="md:flex justify-items-stretch">
